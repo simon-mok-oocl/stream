7ced2215e8c9ae7ca3f11de98423688285000661
moksi3 <simon.mok@oocl.com>
2021-12-01 14:52:09 +0800

    [stream][simon mok ] feat: add reduce

--- src/main/java/com/thoughtworks/collection/StreamReduce.java
+++ src/main/java/com/thoughtworks/collection/StreamReduce.java
@@ -1,18 +1,22 @@
 package com.thoughtworks.collection;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class StreamReduce {
 
     public int getLastOdd(List<Integer> numbers) {
-        return 0;
+        return numbers.stream().filter(number -> number %2 == 1).reduce((first , second) -> second).orElse(-1);
+        //return 0;
     }
 
     public String getLongest(List<String> words) {
-        return null;
+
+        return words.stream().max(Comparator.comparingInt(String::length)).get();
     }
 
     public int getTotalLength(List<String> words) {
-        return 0;
+
+        return words.stream().map(String::length).reduce(0 , Integer::sum);
     }
 }
